7c582572edec32684a658d15d87dca39259b92e5
Vincent the Dev <135163462+VincentAgunda@users.noreply.github.com>
2025-09-27 07:29:42 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/globals.css'
 import { testConnection } from './lib/supabase.js'
@@ -13,6 +14,8 @@ testConnection().then(success => {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
